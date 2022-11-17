e73b6d368d9175ab9601b7b900e6ed6a25d7c779
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-17 21:17:11 +0530

    Fibonacci Series

---
+++ UC-1 Fibonacci Series/Fibonacci Series/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=1, c,n,i;//Declared Variable
+            Console.WriteLine("Enter The Number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for ( i = 0; i < n; i++)
+            {
+                Console.Write($" {a} ");
+                c = a + b;
+                a = b;
+                b = c; 
+            }
+            Console.ReadLine();
+        }
+    }
+}
