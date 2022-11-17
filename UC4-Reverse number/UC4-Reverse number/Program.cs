540ade1c155c7613cee110182dd1180040273934
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-17 21:20:22 +0530

    Reverse number

---
+++ UC4-Reverse number/UC4-Reverse number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_Reverse_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Variable Declare
+            int num;
+            int reverse = 0;
+
+            Console.WriteLine("Enter the number : ");
+            num = int.Parse(Console.ReadLine());
+
+            while (num > 0)
+            {
+                reverse = reverse * 10 + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine($"Reversed number {reverse}");
+            Console.ReadLine();
+           
+        }   
+    }   
+}
