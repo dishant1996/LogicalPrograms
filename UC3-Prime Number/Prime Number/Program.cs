17d151b61e549adf9139a4c77e17c2bb994be1bc
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-17 21:19:27 +0530

    Prime Number

---
+++ UC3-Prime Number/Prime Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //local Variable
+            int num,i, count=0;
+            Console.WriteLine("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)//Condition check
+                Console.WriteLine("Prime Number");
+            else
+                Console.WriteLine("Not Prime Number");
+            Console.ReadLine(); 
+        }
+    }
+}
