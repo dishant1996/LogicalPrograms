fe869049f6dc076d3c0b14b5266113e655fbc8dc
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-17 21:22:50 +0530

    Simulate Stopwatch Program

---
+++ UC6-Simulate Stopwatch Program/UC6-Simulate Stopwatch Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UC6_Simulate_Stopwatch_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Choose the option 1 to start : ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            if (start == 1)
+            {
+                // Begin timing
+                stopwatch.Start();
+            }
+
+            // Do something
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("Choose the option 2 to start : ");
+            int stop = Convert.ToInt32(Console.ReadLine());
+            if (stop == 2)
+            {
+                // Stop timing
+                stopwatch.Stop();
+            }
+
+            // Write result
+            Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+            Console.ReadLine(); 
+        }
+    }
+}
